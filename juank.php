0f11e3592c5115ff652539550f70452e5a0cbcac
Paula Lineros <pau.lokilla@gmail.com>
2013-11-17 22:05:42 -0300

    Update juank.php
    agregar repositorio

--- juank.php
+++ juank.php
@@ -33,6 +33,7 @@
 		<li class="datos">Edad: 22 años</li>
 		<li class="datos">Carrera: Ingeniería Civil en Computación mención Informática</li>
 		<li class="datos">Comuna: Maipú</li>
+		<a href ="http://github.com/juankp"<li class="datos">GitHub </li></a>
 	</center>
 	</ol>
 	</ul>
